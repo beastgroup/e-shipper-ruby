0281f923bd60676510e592b45d8c2a3049948bfa
Eliot Hills <j.eliot.hills@gmail.com>
2018-12-12 16:09:29 -0500

    Add skuCode to Item

--- lib/e_shipper_ruby/classes/item.rb
+++ lib/e_shipper_ruby/classes/item.rb
@@ -1,7 +1,7 @@
 module EShipper
   class Item < EShipper::OpenStruct
 
-    POSSIBLE_FIELDS = [:code, :description, :originCountry, :quantity, :unitPrice]
+    POSSIBLE_FIELDS = [:code, :description, :originCountry, :quantity, :unitPrice, :skuCode]
 
     REQUIRED_FIELDS = POSSIBLE_FIELDS
   end
